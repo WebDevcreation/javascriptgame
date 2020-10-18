8515285b60c60bca244631a4da00b23cc4644f37
root <rishim9817@gmail.com>
2020-10-18 11:07:42 +0000

    I have designed the interface with bootstrap and css files. Also made a seperate js file for understanding purposes.

---
+++ index.js
@@ -0,0 +1,52 @@
+function generate()
+{
+	var num = Math.random()*1000000;
+	num = Math.round(num);
+	localStorage.setItem("RandVal",num);
+	document.getElementById("num").innerHTML = localStorage.getItem("RandVal");
+	document.getElementById("ref").innerHTML = "Refreshing the page in 5 seconds...";
+	setTimeout(function(){
+		if($('#one').css("display","block"))
+		{
+			$('#one').css("display","none");
+			$('#two').css("display","block");
+		}
+	}, 5000);
+}
+function check()
+{
+	var chk = document.forms["form"]["valchk"].value;
+	if(chk=="")
+		alert("Enter a number");
+	else if(chk.length!=6)
+		alert("Enter a six digit number");
+	else
+	{
+		var temp = localStorage.getItem("RandVal");
+		var count=0;
+		for(i=0;i<6;i++)
+		{
+			if(temp[i]==chk[i])
+				count++;
+		}
+		document.getElementById("res").style.display = "block";
+		if (count==6)
+			document.getElementById("res").innerHTML = " Correct Guess";
+		else if(count==0 || count==1)
+			document.getElementById("res").innerHTML = count+" correct guess";
+		else
+			document.getElementById("res").innerHTML = count+" correct guesses";
+	}
+	var $form = $('form');
+	$form.submit(function(){
+		$.post($(this).attr('action'), $(this).serialize(), function(response){
+       	// Nothing to be printed on success. Left blank intentionally.
+		},'json');
+		return false;
+	});
+}
+function makeChange()
+{
+	window.location.reload(true);
+	document.getElementById("res").style.display = "none";
+}
